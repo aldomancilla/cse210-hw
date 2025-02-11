e4d9633c1fec1b7ad5f2aad28d8dd299d4b6d811
aldomancilla <man24023@byui.edu>
2025-02-11 11:24:07 -0600

    Homework

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("Going to sleep for a second...");
+        
+        Console.Write("+");
+        Console.Write("\b \b"); // Erase the + character
+        Console.Write("-"); // Replace it with the - character
+        Thread.Sleep(10000);
+     
+
+        Console.WriteLine("I'm back!!");
+
     }
 }
