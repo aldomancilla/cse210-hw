3078b2e2b87a88026078a96089ed75a1eb1756bd
aldomancilla <man24023@byui.edu>
2025-02-19 21:01:23 -0600

    week7 assignament

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,25 @@
+using System;
+abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    protected Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes() => _minutes;
+    public DateTime GetDate() => _date;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_minutes} min): " +
+               $"Distance {GetDistance():F1} km, Speed {GetSpeed():F1} kph, Pace {GetPace():F2} min per km";
+    }
+}
